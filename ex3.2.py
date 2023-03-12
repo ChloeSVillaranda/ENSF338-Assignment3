5100b7aa00f04bc2a9d60ef46f8721ee7311cab8
BlubziDoodle <68242201+BlubziDoodle@users.noreply.github.com>
2023-03-12 08:33:14 -0600

    finished ex 2.

--- ex3.2.py
+++ ex3.2.py
@@ -1,5 +1,6 @@
 import json
 import time
+import matplotlib.pyplot as plt
 
 # Load the array from the ex2data.json file
 with open('ex2data.json', 'r') as f:
@@ -32,6 +33,8 @@ def binary_search(array, target, start_midpoint=None):
     return False
 
 # Time the performance of each search task using the tweaked binary search with midpoints at 1/5, 2/5, 3/5, and 4/5 of the array
+results = []
+
 for task in search_tasks:
     best_midpoint = None
     best_time = float('inf')
@@ -39,17 +42,49 @@ for task in search_tasks:
 
     midpoints = [len(array)//5, 2*len(array)//5, 3*len(array)//5, 4*len(array)//5]
 
-    for i, mid in enumerate(midpoints):
+    for mid in midpoints:
         start_time = time.perf_counter()
         result = binary_search(array, task, start_midpoint=mid)
         end_time = time.perf_counter()
 
         if end_time - start_time < best_time:
             best_time = end_time - start_time
-            best_midpoint = f"{i+1}/5"
+            best_midpoint = mid/len(array)
 
         if result:
             found = True
             break
 
+    results.append((task, best_midpoint, found, best_time))
+
+for task in search_tasks:
+    best_midpoint = None
+    best_time = float('inf')
+    found = False
+
+    midpoints = [len(array)//5, 2*len(array)//5, 3*len(array)//5, 4*len(array)//5]
+
+    for mid in midpoints:
+        start_time = time.perf_counter()
+        result = binary_search(array, task, start_midpoint=mid)
+        end_time = time.perf_counter()
+
+        if end_time - start_time < best_time:
+            best_time = end_time - start_time
+            best_midpoint = mid/len(array)
+
+        if result:
+            found = True
+            break
+
+    results.append((task, best_midpoint, found, best_time))
+
     print(f"Search for {task}: {'Found' if found else 'Not Found'} (Best Midpoint: {best_midpoint}, Best Time: {best_time:.6f} seconds)")
+
+
+# Plot a scatterplot of task versus best midpoint
+plt.scatter([task for task, midpoint, found, time in results], [midpoint for task, midpoint, found, time in results])
+plt.xlabel('Task')
+plt.ylabel('Best Midpoint')
+plt.title('Task vs Best Midpoint')
+plt.show()
